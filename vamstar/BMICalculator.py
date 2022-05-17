b2a86f206db79797e4c837a7ef05479ba27885ae
abhisharmaa <abhisheks982@gmail.com>
2022-05-17 01:36:21 +0100

    Add files via upload

--- vamstar/BMICalculator.py
+++ vamstar/BMICalculator.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
 
-
+#Calculation logic for BMI
 def calculate_BMI_RISK(df):
     df['Heightmeter']=df.HeightCm/100
     df['BMI']=df.WeightKg/(df.Heightmeter*df.Heightmeter)
     df['BMI']=df.BMI.round(decimals=1)
-    #print(df.dtypes)
-    #df['BMI'] = df['BMI']
     conditions = [ (df['BMI']  <= 18.4),
     (df['BMI']  >= 18.5) & (df['BMI'] <= 24.9),
     (df['BMI'] >= 25.0) & (df['BMI'] <= 29.9),
@@ -19,31 +17,16 @@ def calculate_BMI_RISK(df):
     df['Health_Risk'] = np.select(conditions, Health_risk)
     df['BMI_CATEGORY'] =  np.select(conditions, BMI_CATEGORY)
     df.drop('Heightmeter', axis=1, inplace=True)
-    ## Count Number of Overweight People 
-    
 
-    #print(df)
     return df
 
 
-
-
-    # x=df.merge(df_BMI_category, how='cross')
-    # x=x[(x.BMI>=x.bmi_lower) & (x.BMI<=x.bmi_upper)]
-    #return x[['Gender','HeightCm','WeightKg','BMI','bmi_category','health_risk']]
-
-# This could read data in millions
-
 if __name__ == "__main__":
-    df= pd.read_json('customer.json')
-    print(df)
-    #df_BMI_category=pd.read_csv('config/BMI_category.csv')
     file_name='output/BMIofCustomers.csv'
-    
+    #Read .json file
+    df= pd.read_json('customer.json')  
     customer_health = calculate_BMI_RISK(df)
     cnts = customer_health[customer_health['BMI_CATEGORY'] == 'Overweight' ].shape[0]
     print('Total number of overweight customers = ' + str(cnts))
-    
-    #print(cnts)
     customer_health.to_csv(file_name,index=False)
    
--- vamstar/test_BMICalculator.py
+++ vamstar/test_BMICalculator.py
@@ -4,31 +4,20 @@ import BMICalculator
 
 
 def test_json_to_object_conversion():
+    #Test data
     json_data = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96}]
-    bmi = round(96 / 1.71, 3)
-    health_risk = 'Medium risk'
-    bmi_category = 'Moderately obese'
+    #Expected dataframe after calculation
     expected_df = pd.DataFrame({'Gender': 'Male', 'HeightCm': 171,'WeightKg':96,'BMI': 32.8,'Health_Risk':'Medium risk','BMI_CATEGORY':'Moderately obese'},index=[0])
     print(expected_df)
-    df = pd.DataFrame.from_dict(json_data)
-    #df = pd.DataFrame(list(json_data.items()),columns = ['Gender','HeightCm','WeightKg'])
-    #df = pd.read_json(json_data)
-    
+    df = pd.DataFrame.from_dict(json_data) 
+    #Returned Dataframe  
     Actual_df = BMICalculator.calculate_BMI_RISK(df)
-    print(Actual_df)
-   
-    # if pd.testing.assert_frame_equal(Actual_df, expected_df):
-    #     print('Test Passed')
-    # else:
-    #     print('Test Failed')    
+    print(Actual_df)   
+    #Comparing two dataframes
     assert_frame_equal(Actual_df, expected_df)
     if  assert_frame_equal(Actual_df, expected_df) == None:
         print('Test Passed')
     else:
         print('Test Failed')        
-    
-    #assert BMICalculator.calculate_BMI_RISK(df)['BMI'] == bmi
-    #assert assert_df[assert_df['Health_Risk'] == health_risk]
-    # assert main_oops.create_people_health_list(dictionary)[0].bmi_category == bmi_category
 
 test_json_to_object_conversion()    
